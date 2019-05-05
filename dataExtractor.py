325995370860a1ebc9f66503bb8b52102d9e51b7
Jorge <jorge@Nathans-MacBook-Pro-4.local>
2019-05-05 03:53:08 -0700

    init commit

---
+++ dataExtractor.py
@@ -0,0 +1,107 @@
+import os
+import cv2
+import time
+import numpy as np
+from keras.utils import to_categorical
+from scipy import signal
+
+feature_params = dict(maxCorners=500,
+                      qualityLevel=0.3,
+                      minDistance=7,
+                      blockSize=7)
+
+optical_flow_lk_params = dict(winSize=(10, 10),
+                              maxLevel=2,
+                              criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+
+def dense_optical_flow(frame_one, frame_two):
+    #better results
+    hue_saturation_value = np.zeros_like(frame_one)
+    frame_one = cv2.cvtColor(frame_one, cv2.COLOR_BGR2GRAY)
+    frame_two = cv2.cvtColor(frame_two, cv2.COLOR_BGR2GRAY)
+    hue_saturation_value[..., 1] = 255
+    flow = cv2.calcOpticalFlowFarneback(frame_one, frame_two, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    hue_saturation_value[..., 0] = ang * 180 / np.pi / 2
+    hue_saturation_value[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+    bgr = cv2.cvtColor(hue_saturation_value, cv2.COLOR_HSV2BGR)
+    #cv2.imshow('frame2', bgr)
+    #cv2.waitKey(30) & 0xff
+    #time.sleep(.001)
+    #angle is to miniscule, look into layer for better classifcation maybe but for now use magnitude
+    return mag
+
+
+def lucas_kanae_optical_flow(frame_one, frame_two):
+    # Poor results given
+    frame_two_copy = frame_two.copy()
+    color = np.random.randint(0, 255, (100, 3))
+    mask = np.zeros_like(frame_one)  # create mask using same size and set all values to 255
+    corner_points = cv2.goodFeaturesToTrack(frame_one, mask=None, **feature_params)
+    points_one, st, err = cv2.calcOpticalFlowPyrLK(frame_one, frame_two, corner_points, None, **optical_flow_lk_params)
+    good_points = points_one[st == 1]
+    good_original_points = corner_points[st == 1]
+
+    # viualization
+    for i, (new, old) in enumerate(zip(good_points, good_original_points)):
+        a, b = new.ravel()
+        c, d = old.ravel()
+        mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
+        frame_two_copy = cv2.circle(frame_two_copy, (a, b), 5, color[i].tolist(), -1)
+    img = cv2.add(frame_two_copy, mask)
+
+    # cv2.imshow('frame', img)
+    # cv2.waitKey(30) & 0xff
+    # time.sleep(.0001)
+
+    return points_one
+
+
+def load_dataset(frame_dir, labels_location):
+    print("Loading dataset")
+    return load_frames(frame_dir), load_labels(labels_location)
+
+
+def extract_frames(video_location, output_location):
+    print("Extracting frames from video", video_location)
+    video_capture = cv2.VideoCapture(video_location)
+    success, image = video_capture.read()
+    count = 0
+    while success:
+        success, image = video_capture.read()
+        image_name = output_location + "frame_" + str(count) + ".jpg"
+        cv2.imwrite(image_name, image)
+        count += 1
+    print("Done saving frames")
+
+# Load frames in black and white image numpy array
+
+
+def load_frames(frame_dir):
+    test_loading = 0
+    test_loading = -999998
+    print("Loading frames from:", frame_dir)
+    images = []
+    filelist = os.listdir(frame_dir)
+    filelist = sorted(filelist, key=lambda x: int((os.path.splitext(x))[0].split('_')[1]))
+    for filename in filelist:
+        if test_loading > 900:
+            break
+        test_loading += 1
+        file_location = frame_dir + filename
+        images.append(cv2.imread(file_location, 1))
+    return images
+    #return np.array(images)
+
+
+def load_labels(labels_location):
+    print("Loading labels from:", labels_location)
+    labels = open(labels_location, "r")
+    train_labels = [float(label) for label in labels ]
+    return np.array(train_labels)
+
+
+def one_hot_encode(labels):
+    return to_categorical(labels, num_classes=len(labels))
+
---
+++ main.py
@@ -0,0 +1,75 @@
+import numpy as np
+from keras import Sequential
+from keras.optimizers import Adam
+from keras.layers import Dense, Flatten
+from dataExtractor import load_dataset
+from dataExtractor import extract_frames
+from dataExtractor import lucas_kanae_optical_flow, dense_optical_flow
+
+
+# Generate the optical flow data using dense optical flow
+# for each frame where flow = |diff[img_i - img_i+1]| -> u,v, u is mag v is angle, v is currently discarded
+def generate_optical_flow_data(images):
+    print("Generating optical flow data using Dense optical flow algorithm")
+    data = []
+    num_images = int(len(images) / 2)
+    for i in range(num_images):
+        print(i,"/", num_images)
+        # data.append(lucas_kanae_optical_flow(images[i], images[i+1]))  gives poor results
+        data.append(dense_optical_flow(images[i], images[i+1]))
+    return data
+
+
+def load_training_data():
+    frame_dir_location = "data/train_frames/"
+    labels_location = "data/train_labels/train.txt"
+    return load_dataset(frame_dir_location, labels_location)
+
+
+def generate_model():
+    model = Sequential()
+
+    #input and flatten
+    model.add(Dense(20, kernel_initializer='normal', activation='relu', name="input", input_shape=(20,480,640)))
+    model.add(Flatten()) # Flatten reuslts to 1-d vec
+
+    # hidden layers
+    model.add(Dense(10, kernel_initializer='normal', activation='relu', name="first_hidden"))
+    model.add(Dense(5, kernel_initializer='normal', activation='relu', name="second_hidden"))
+
+    #output layer
+    model.add(Dense(1,kernel_initializer='normal', activation='linear', name="output_layer", input_dim=1))
+    optimizer = Adam(lr=0.0001)
+    model.compile(loss='mean_absolute_error', optimizer=optimizer, metrics=['mean_absolute_error'])
+    model.summary()
+    return model
+
+
+def train_model(train_data, train_labels, model):
+    train_data_one = np.array([train_data[:20]])
+    test = []
+    train = []
+    i = 0
+    # train on all but last 2 frames seconds
+    for _ in range():
+        train.append(np.array(train_data[i:i+20]))
+        i += 20
+        test.append(train_labels[_])
+    train = np.array(train)
+    # test on next 30 seconds
+    test = np.array(test)
+    ex = np.array([train_labels[0]])
+    model.fit(train, test, batch_size=1, epochs=8, class_weight=None)
+    print(model.predict(train_data_one))
+
+
+def main():
+    print("Starting program")
+    # train_vid_location = "videos/train.mp4"
+    # extract_frames(train_vid_location, frame_dir_location)
+    train_data, train_labels = load_training_data()
+    train_data = generate_optical_flow_data(train_data)
+    model = generate_model()
+    model = train_model(train_data, train_labels, model)
+
+main()
